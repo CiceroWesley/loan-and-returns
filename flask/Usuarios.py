cf94352d1bccadb62eb2e92181d37c2e1905fdfb
Wesley <wesleycariutaba@gmail.com>
2021-11-30 13:48:00 -0300

    cadastro usuario e equipamento

--- flask/Usuarios.py
+++ flask/Usuarios.py
@@ -4,6 +4,5 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), unique=False, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    cpf = db.Column(db.String(200), unique=True, nullable=True)
-    password = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=True)
+    password = db.Column(db.String(80), unique=False, nullable=False)
